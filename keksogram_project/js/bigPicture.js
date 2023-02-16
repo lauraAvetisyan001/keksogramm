2730d661ad2825a9bfc205c4dd8f89ea751b3a6d
mac <mac@MacBook-Pro-MacBook.local>
2023-02-16 19:16:57 +0200

    pre

--- keksogram_project/js/bigPicture.js
+++ keksogram_project/js/bigPicture.js
@@ -6,11 +6,15 @@ const bigPicture = document.querySelector('.big-picture'),
       commentsCount = document.querySelector('.comments-count'),
       imgDescription = document.querySelector('.social__caption'),
       socialComment = document.querySelector('.social__comment'),
-      socialComments = document.querySelector('.social__comments'),      
+      socialComments = document.querySelector('.social__comments'),     
+      socialCommentsCount = document.querySelector('.social__comment-count'), 
+      commentsBtn = document.querySelector('.social__comments-loader'),    
       pictureCancelButton = document.querySelector('#picture-cancel');
 
 
-let post; 
+let post;  
+let currentCommentsCount = 5;
+let comSlice;
 
 export function showPopup(){
   pictures.addEventListener('click', (evt)=>{
@@ -18,17 +22,45 @@ export function showPopup(){
     const photoId = +evt.target.dataset.id;
     post = data.find(data => data.id === photoId);
     showComments();
-    showBigImg();
+    showBigImg(); 
+
   })};  
 
+  function showCurrentComments(){ 
+    if(data.comments.length - currentCommentsCount > 5){
+      currentCommentsCount = currentCommentsCount + 5
+    } else {
+      currentCommentsCount = data.comments.length
+    }
+  }
+ 
   
+
+  function addComments(){
+    comSlice = post.comments.slice(0, currentCommentsCount);
+    socialCommentsCount.textContent = `${comSlice.length} из ${post.comments.length} комментарів`
+    console.log(comSlice)
+  }
+
+
+
+  commentsBtn.addEventListener('click', () => {
+    showCurrentComments()
+    addComments()
+
+  });
+
+
+
 function showBigImg(){
   bigPicture.classList.remove('hidden');
   bigPictureImg.src = post.url;
   likesCount.textContent = post.likes;
   commentsCount.textContent = post.comments.length;
   imgDescription.textContent = post.description;
+
 }
+
 
 const commentsFragment = new DocumentFragment();
 
@@ -38,12 +70,20 @@ function showComments(){
     socialCommentClone.querySelector('.social__text').textContent = comment.message
     socialCommentClone.querySelector('.social__picture').alt = comment.name
     socialCommentClone.querySelector('.social__picture').src = comment.avatar
-    commentsFragment.append(socialCommentClone)
+    commentsFragment.append(socialCommentClone); 
   })
   socialComments.textContent = '';
   
-  socialComments.append(commentsFragment)
+  socialComments.append(commentsFragment);
 }
+
+
+
+      
+
+
+
+
 
 
 pictureCancelButton.addEventListener('click', ()=>{
@@ -59,4 +99,7 @@ function escBigPhoto(e){
   }
 } 
 
+
 document.addEventListener('keydown', escBigPhoto);
+
+export{post};
